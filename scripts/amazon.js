eace95c3becaca6160ebae4c77056dbfa6249a9a
suf@dev <suf@dev>
2025-08-20 19:30:07 +0530

    Make the  cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -78,8 +78,14 @@ if (matchingItem) {
  });
 }
 
- 
- console.log(cart);
+let cartQuantity = 0;
+
+cart.forEach((item) =>{
+cartQuantity += item.quantity;
+});
+
+document.querySelector('.js-cart-quantity')
+.innerHTML = cartQuantity;
  
 });
 });
